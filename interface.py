b18557cf9c2e16023058d4359c6c49c4c10c4b16
Miguel Fiaschi <mfiaschi36@gmail.com>
2026-06-19 13:03:10 -0300

    feat: aparição dos shapes dos pesos confirmando que carregou tudo

--- interface.py
+++ interface.py
@@ -1,4 +1,16 @@
 import tkinter as tk
+import numpy as np
+
+W1 = np.load("W1.npy")
+b1 = np.load("b1.npy")
+W2 = np.load("W2.npy")
+b2 = np.load("b2.npy")
+W3 = np.load("W3.npy") 
+b3 = np.load("b3.npy")
+
+print("Pesos e vieses carregados")
+print(f"W1: {W1.shape}, W2: {W2.shape}, W3: {W3.shape}")
+
 
 janela = tk.Tk()
 janela.title("MFNet - Reconhecedor de Dígitos")
@@ -21,6 +33,22 @@ def limpar():
 
 def prever():
     print("Botão Prever clicado! (ainda sem IA conectada)")
+
+def relu(x):
+    return np.maximum(0, x) 
+
+def softmax(x):
+    exp = np.exp(x - x.max(axis=1, keepdims=True))
+    return exp / exp.sum(axis=1, keepdims=True)
+
+def forward(X):
+    z1 = np.dot(X, W1) + b1
+    a1 = relu(z1)
+    z2 = np.dot(a1, W2) + b2
+    a2 = relu(z2)
+    z3 = np.dot(a2, W3) + b3
+    a3 = softmax(z3)
+    return a3
 
 # Frame para os botões ficarem lado a lado
 frame_botoes = tk.Frame(janela)
