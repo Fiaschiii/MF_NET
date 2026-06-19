c75972b58fba40986dd0c6c7cbc3aa30e05bb0cb
Miguel Fiaschi <mfiaschi36@gmail.com>
2026-06-19 00:07:29 -0300

    feat: canvas de desenho com botões prever e limpar

--- interface.py
+++ interface.py
@@ -1,20 +1,20 @@
 import tkinter as tk
 
 janela = tk.Tk()
-janela.title("MFNET - Reconhecedor de Dígitos")
+janela.title("MFNet - Reconhecedor de Dígitos")
 
-canvas = tk.Canvas(janela, width = 280, height = 280, bg = "black")
-canvas.pack(pady = 10)
-
+canvas = tk.Canvas(janela, width=280, height=280, bg="black")
+canvas.pack(pady=10)
 
 def desenhar(event):
+    print(f"Mouse em: {event.x}, {event.y}")  # debug
     x, y = event.x, event.y
     raio = 8
     canvas.create_oval(
         x - raio, y - raio, x + raio, y + raio,
-        fill = "white", outline = "white"
+        fill="white", outline="white"
     )
 
-    canvas.bind("<B1-Motion>", desenhar)
+canvas.bind("<B1-Motion>", desenhar)
 
-    janela.mainloop()
+janela.mainloop()
