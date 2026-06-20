389142d2c0a422f63f7310ead117d9e2844813bc
Miguel Fiaschi <mfiaschi36@gmail.com>
2026-06-19 21:03:17 -0300

    feat: conectar MFNet treinada ao canvas de desenho

--- interface.py
+++ interface.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import numpy as np
+from PIL import Image, ImageDraw
+
 
 W1 = np.load("W1.npy")
 b1 = np.load("b1.npy")
@@ -20,19 +22,40 @@ canvas.pack(pady=10)
 
 def desenhar(event):
     x, y = event.x, event.y
-    raio = 8
+    raio = 14
     canvas.create_oval(
         x - raio, y - raio, x + raio, y + raio,
         fill="white", outline="white"
     )
 
+    desenho_pil.ellipse(
+        [x - raio, y - raio, x + raio, y - raio],
+        fill = 255
+    )
+
+
 canvas.bind("<B1-Motion>", desenhar)
 
 def limpar():
     canvas.delete("all")
+    desenho_pil.rectangle([0, 0, 280, 280], fill=0)
 
 def prever():
-    print("Botão Prever clicado! (ainda sem IA conectada)")
+    imagem_pequena = imagem_pil.resize((28, 28))
+    array = np.array(imagem_pequena)
+
+    array = array / 255.0
+    array = array.reshape(1, 784)
+
+
+    probabilidade = forward(array)
+    digito_previsto = np.argmax(probabilidade)
+    confianca = probabilidade[0][digito_previsto] * 100
+
+    print(f"\nPrevisão: {digito_previsto} ({confianca:.1f}% confiança)")
+    print(f"Todas as propabilidades:")
+    for i in range(10):
+        print(f"  {i}: {probabilidade[0][i]*100:.1f}%")
 
 def relu(x):
     return np.maximum(0, x) 
@@ -59,6 +82,9 @@ botao_prever.pack(side="left", padx=5)
 
 botao_limpar = tk.Button(frame_botoes, text="Limpar", command=limpar, width=10)
 botao_limpar.pack(side="left", padx=5)
+
+imagem_pil = Image.new("L", (280, 280), color=0)
+desenho_pil = ImageDraw.Draw(imagem_pil)
 
 
 janela.mainloop()
